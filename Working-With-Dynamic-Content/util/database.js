14b48eb977339333f74a5a4c58894a5727269267
j22868706 <j22868706@gmail.com>
2024-11-11 23:58:07 -0600

    added user

--- Working-With-Dynamic-Content/util/database.js
+++ Working-With-Dynamic-Content/util/database.js
@@ -1,23 +1,10 @@
-// const mysql = require('mysql2');
 
-// const pool = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'node-complete',
-//     password: 'root1234'
-// });
-
-// module.exports = pool.promise();
 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(
-    'node-complete',
-    'root',
-    'root1234',
-    {
-        dialect: 'mysql',
-        host: 'localhost'
-    }
-);
+
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
 
 module.exports = sequelize;
---
+++ Working-With-NoSQL/app.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const mongoConnect = require('../Working-With-NoSQL/util/database').mongoConnect;
+const User = require('./models/user');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById("6732e6e82da6d25eb7737c5a")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+
+  app.listen(3000);
+})
